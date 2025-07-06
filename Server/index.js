0305d2725c205391c8eb54cd2cbfa355b740691c
Shivaanjay Narula <shivaanjaynarula@gmail.com>
2025-07-06 15:02:13 +0530

    fixes

--- Server/index.js
+++ Server/index.js
@@ -10,7 +10,7 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:3000", 
   process.env.URL, 
-  "https://z1ppie.vercel.app" // Add your frontend URL here
+  "https://zippy-two.vercel.app/" // Add your frontend URL here
 ];
 app.use(
   cors({
